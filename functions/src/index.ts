1786625dd069046f83c691f1ba0b785261084247
Brendon Earl <bearl@uvic.ca>
2018-11-06 16:34:32 -0800

    move and organize api index and top-level index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,3 @@
-import * as express from "express";
-import * as bodyParser  from 'body-parser';
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 import * as admin from "firebase-admin";
 // The Firebase Functions library
@@ -7,22 +5,6 @@ import * as functions from "firebase-functions";
 
 admin.initializeApp(functions.config().firebase);
 
-// Routes
-import {authRouter} from "./api/auth"
+import api from 'api';
 
-const app = express();
-// https://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
-app.disable("x-powered-by");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-// Any requests to /api/users will be routed to the user router!
-app.use("/auth", authRouter);
-
-// Again, lets be nice and help the poor wandering servers, any requests to /api
-// that are not /api/users will result in 404.
-app.get("*", async (req: express.Request, res: express.Response) => {
-	res.status(404).send("This route does not exist.");
-});
-
-exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(api);
