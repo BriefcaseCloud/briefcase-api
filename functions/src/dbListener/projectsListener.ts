b5e28635d34329de656b0eb02651f53765602586
Dylan Brown <dylanbrown@uvic.ca>
2018-11-28 18:30:44 -0800

    fuuu

--- functions/src/dbListener/projectsListener.ts
+++ functions/src/dbListener/projectsListener.ts
@@ -8,11 +8,12 @@ export const updateFunction = functions.firestore.document('/projects/{projectid
 .onWrite((change, context) => {
     // console.log("here")
     let userChange = {}
-    // const newUsersData = change.after.data()
-    // const oldUsersData = 
+    const newUsersData = change.after.data()
+    const oldUsersData = change.before.data()
     // console.log(newUsersData.details.user)
-    const newUsers = change.after.data().users;
-    const oldUsers = change.after.data().users;
+    const newUsers = newUsersData.users;
+    const oldUsers = oldUsersData.users;
+    console.log("kjfjsdk")
     if(oldUsers && newUsers.length != oldUsers.length) {
         const newestUser = newUsers[newUsers.length -1].user
         userChange = {id: newestUser, project: context.params.projectid} 
