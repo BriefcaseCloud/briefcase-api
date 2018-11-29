2352d8dfc83928f8428955383390bb0f21329ee3
Dylan Brown <dylanbrown@uvic.ca>
2018-11-28 22:51:17 -0800

    fix lint errors

--- functions/src/dbListener/projectsListener.ts
+++ functions/src/dbListener/projectsListener.ts
@@ -5,20 +5,19 @@ import * as functions from "firebase-functions";
 import * as usersStorage from "../storage/usersStorage";
 
 export const updateFunction = functions.firestore.document('/projects/{projectid}')
-.onWrite((change, context) => {
+.onWrite(async (change, context) => {
     // console.log("here")
     let userChange = {}
-    const newUsersData = change.after.data()
-    const oldUsersData = change.before.data()
+    // const newUsersData = change.after.data()
+    // const oldUsersData = change.before.data()
     // console.log(newUsersData.details.user)
-    const newUsers = newUsersData.users;
-    const oldUsers = oldUsersData.users;
+    const newUsers = change.after.data().users;
+    const oldUsers = change.before.data().users;
     console.log("kjfjsdk")
-    if(oldUsers && newUsers.length != oldUsers.length) {
+    if(oldUsers && newUsers.length !== oldUsers.length) {
         const newestUser = newUsers[newUsers.length -1].user
         userChange = {id: newestUser, project: context.params.projectid} 
     }
-    usersStorage.updateUserProjects(userChange).then((response)=> {
-        return response;
-    })
+    const updatedUser = await usersStorage.updateUserProjects(userChange)
+    return updatedUser;
 });
