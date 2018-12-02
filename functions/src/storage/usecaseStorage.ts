b19e79528666c839ec4df8f98f9804a72e6e4565
Brendon Earl <bearl@uvic.ca>
2018-12-01 22:35:48 -0800

    Tidy, follow conventions, and document usecase files

--- functions/src/storage/usecaseStorage.ts
+++ functions/src/storage/usecaseStorage.ts
@@ -6,21 +6,12 @@ const db = admin.firestore()
  **     Storage     **
  *********************/
 
-export async function deleteAllUseCases(puid) {
-  const path = `projects/${puid}/usecases`
-  const snapshot = await db.collection(path).get()
-  return snapshot.docs.map(doc => doc.ref.delete())
-}
 
-export async function deleteUseCase(puid, usecaseid) {
-  return db
-    .collection('projects')
-    .doc(puid)
-    .collection('usecases')
-    .doc(usecaseid)
-    .delete()
-}
-
+/**
+ * Add use case to project
+ * @param puid - project unique identifier
+ * @param usecase - usecase content to save
+ */
 export async function addUseCase(puid, usecase) {
   return db
     .collection('projects')
@@ -28,4 +19,28 @@ export async function addUseCase(puid, usecase) {
     .collection('usecases')
     .doc()
     .set(usecase)
+}
+
+/**
+ * Delete usecase on project
+ * @param puid - project unique identifier
+ * @param ucuid - usecase unique identifier
+ */
+export async function deleteUseCase(puid, ucuid) {
+  return db
+    .collection('projects')
+    .doc(puid)
+    .collection('usecases')
+    .doc(ucuid)
+    .delete()
+}
+
+/**
+ * Delete all usecase in project
+ * @param puid - project unique identifier
+ */
+export async function deleteAllUseCases(puid) {
+  const path = `projects/${puid}/usecases`
+  const snapshot = await db.collection(path).get()
+  return snapshot.docs.map(doc => doc.ref.delete())
 }
