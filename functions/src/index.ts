73b9c80fa9e580d276bb1006d9767c70d5ac6553
Brendon Earl <bearl@uvic.ca>
2018-12-03 10:17:02 -0800

    Try adding cors header

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,6 +16,11 @@ const app = express()
 
 // https://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable('x-powered-by')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
