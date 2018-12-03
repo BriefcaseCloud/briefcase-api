a9f141679fb4a6b1fc5c37feb39151e7c703e3e2
Brendon Earl <bearl@uvic.ca>
2018-12-02 16:18:43 -0800

    Add get users's project in usersLogic

--- functions/src/logic/usersLogic.ts
+++ functions/src/logic/usersLogic.ts
@@ -3,6 +3,7 @@ import * as express from 'express'
 // Internal Dependencies
 import * as usersStorage from '../storage/usersStorage'
 import * as authStorage from '../storage/authStorage'
+import * as projectsStorage from '../storage/projectsStorage'
 
 /*********************
  **      Logic      **
@@ -71,6 +72,31 @@ export async function removeUser(req: express.Request, res: express.Response) {
     .deleteUser(uuid)
     .then(() => authStorage.deleteUser(uuid))
     .then(() => res.status(200).send(`User ${uuid} deleted`))
+    .catch(err => {
+      console.error(err)
+      return res.status(500).send('Server Error')
+    })
+}
+
+
+/**
+ * Get projects for user
+ * @param req - express request object
+ * @param req.params - request url params
+ * @param req.params.uuid - unique user id to get projects for
+ * @param res - express response object
+ */
+export async function getProjects(req: express.Request, res: express.Response) {
+  const { uuid } = req.params
+  return usersStorage
+    .readUser(uuid)
+    .then((data) =>
+      data.projects.map((puid) => ({
+        ...projectsStorage.getProjectDetails(puid),
+        puid
+      }))
+    )
+    .then((projects) => res.status(200).send({projects}))
     .catch(err => {
       console.error(err)
       return res.status(500).send('Server Error')
